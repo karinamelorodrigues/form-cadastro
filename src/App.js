5d7bc0ab31705ac0c3a8b33a06a42058862410a7
Karina Melo Rodrigues <karina.rodrigues@nb-will0576.avista.local>
2021-12-01 12:20:47 -0300

    Criaçao do formulario utilizando a biblioteca Material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
+import { Container, Typography } from '@material-ui/core';
+import "@fontsource/roboto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container component="article" maxWidth="sm">
+      <Typography 
+        variant="h3" 
+        component="h1" 
+        align="center"
+      >
+        Formulário de Cadastro
+      </Typography>
+      <FormularioCadastro/>
+    </Container>
   );
 }
 
